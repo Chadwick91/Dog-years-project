fb8359ec95419d642065bc327ec223122c2231a7
Chadwick91 <89036537+Chadwick91@users.noreply.github.com>
2024-04-22 22:10:50 -0600

    Copy main.js
    Copy from console

---
+++ main.js
@@ -0,0 +1,24 @@
+var myAge = 32
+// This is assigning the variable 'myAge' with the number 32
+
+var earlyYears = 2;
+// This is assigning the variable 'early years' with the number 2
+
+earlyYears *= 10.5;
+
+myAge -= 2;
+
+var laterYears = 30;
+
+laterYears *= 4;
+// Times equals takes the variable "laterYears" and multiplies it by the number indicated
+
+var myAgeInDogYears = earlyYears + laterYears;
+// this variable eqauls the values of these two strings added together
+
+var myName = 'Chad'.toLowerCase();
+// This built in method will insure the variable 'myName' will always log as all lowercase
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
